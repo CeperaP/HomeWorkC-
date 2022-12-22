82437b544719d7241da9e15aa5e19f1270f77e02
Sergey <rogozhinis@mail.ru>
2022-12-22 21:50:04 +0500

    задача8

---
+++ hmwrk_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8. Напишите программу,которая на вход принимает число (N),
+// а на выходе,показывает все четные числа от 1 до N.
+
+Console.WriteLine("ВВедите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int numberA = 0;
+for (numberA = 0; numberA <= N; numberA= numberA=numberA+2)
+{
+Console.WriteLine(numberA);
+}
